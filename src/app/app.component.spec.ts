16029f961d97cc5b9a6d849ade8ade5a9ce23c8f
Chin-Chau Low <cc@lows.sg>
2016-08-03 08:00:39 +0800

    Update to rc4 and cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,14 @@
 import {
-  it,
   inject,
-  describe,
-  beforeEachProviders,
-  expect
+  addProviders,
 } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('App', () => {
-  beforeEachProviders(() => [
-    AppComponent
-  ]);
+  beforeEach(() => addProviders([
+    AppComponent,
+  ]));
   it ('should work', inject([AppComponent], (app: AppComponent) => {
     // Add real test here
-    expect(2).toBe(2);
+    expect(app).toBeDefined();
   }));
 });
