3fdcc07d76f6e11c4659778d6e5f4feca14432d7
Chin-Chau Low <cc@lows.sg>
2016-09-08 16:08:31 +0800

    Reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-<h1>Angular 2 App with Webpack</h1>
-`,
+    <h1>Angular 2 App with Webpack</h1>
+  `,
   styles: [`
-`],
+  `],
   // templateUrl: 'app.component.html',
   // styleUrls: [ 'app.component.css' ],
 })
